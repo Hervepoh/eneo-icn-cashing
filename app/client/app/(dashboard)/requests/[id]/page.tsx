8c257363a38bd2d531c2288482adacea3477b321
Ngando Epoh Herve <epohherve63@gmail.com>
2024-08-17 11:47:11 +0100

    IMPROVE search bill layout

--- app/client/app/(dashboard)/requests/[id]/page.tsx
+++ app/client/app/(dashboard)/requests/[id]/page.tsx
@@ -1,31 +1,16 @@
 "use client"
+import { useRef, useState } from "react";
+import { useParams } from "next/navigation";
+import { Loader2, SearchCheckIcon, SearchCode, SearchIcon, SearchSlash, SearchX } from "lucide-react";
+import { BiSearch } from "react-icons/bi";
 
 import {
     Card,
     CardContent,
     CardDescription,
-    CardFooter,
     CardHeader,
     CardTitle,
 } from "@/components/ui/card";
-import {
-    ResizableHandle,
-    ResizablePanel,
-    ResizablePanelGroup,
-} from "@/components/ui/resizable"
-import { Button } from '@/components/ui/button';
-import { Skeleton } from "@/components/ui/skeleton"
-import { DataTable } from '@/components/data-table';
-import { useRef, useState } from "react";
-import { useParams } from "next/navigation";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Input } from "@/components/ui/input";
-import { ScrollArea } from "@/components/ui/scroll-area"
-import { Separator } from "@/components/ui/separator"
-import { cn } from "@/lib/utils";
-import { SearchForm } from "@/features/search/components/search-form";
-import { SearchCheckIcon, SearchCode, SearchIcon, SearchSlash, SearchX } from "lucide-react";
-import { BiSearch } from "react-icons/bi";
 import {
     Select,
     SelectContent,
@@ -33,8 +18,6 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select"
-import { FormLabel } from "@/components/ui/form";
-import { Label } from "@/components/ui/label";
 import {
     Table,
     TableBody,
@@ -45,7 +28,25 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table"
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+} from "@/components/ui/resizable"
+import { Label } from "@/components/ui/label";
+import { Skeleton } from "@/components/ui/skeleton"
+import { Checkbox } from "@/components/ui/checkbox";
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { DataTable } from './_components/data-table';
+
+import { cn } from "@/lib/utils";
+
 import { useGetRequest } from "@/features/requests/api/use-get-request";
+import { SearchByInvoiceForm } from "@/features/search/components/search-by-invoice-form";
+import { SearchByContractForm } from "@/features/search/components/search-by-contract-form";
+import { SearchByRegroupForm } from "@/features/search/components/search-by-regroup-form";
+import { SearchByCodeCliForm } from "@/features/search/components/search-by-codecli-from";
+import { columns } from "./_components/columns";
 
 interface Invoice {
     id: string;
@@ -54,36 +55,19 @@ interface Invoice {
     amount: number;
 }
 
-type Props = {}
-const tags = Array.from({ length: 50 }).map(
-    (_, i, a) => `v1.2.0-beta.${a.length - i}`
-)
-
-
-
-
 export default function TransactionsDetails() {
     const params = useParams<{ id: string }>();
     const { isLoading, isError, data, error } = useGetRequest(params.id)
+    const [searchError, setSearchError] = useState("")
+    const [isFirstView, setIsFirstView] = useState(true);
 
+    const [searchIsLoading, setSearchIsLoading] = useState(false)
 
     const [invoiceNumber, setInvoiceNumber] = useState('');
 
     const [selectedInvoices, setSelectedInvoices] = useState<Invoice[]>([]);
-    const [invoices, setInvoices] = useState<Invoice[]>([
-        { id: '1', number: 'INV-001', contract: 'INV-001', amount: 500 },
-        { id: '2', number: 'INV-002', contract: 'INV-001', amount: 750 },
-        { id: '3', number: 'INV-003', contract: 'INV-001', amount: 300 },
-        { id: '4', number: 'INV-004', contract: 'INV-001', amount: 1000 },
-    ]);
-
-    const handleSearch = () => {
-        // Recherche des factures par numéro de facture
-        const filteredInvoices = invoices.filter((invoice) =>
-            invoice.number.includes(invoiceNumber)
-        );
-        setInvoices(filteredInvoices);
-    };
+    //const [invoices, setInvoices] = useState<Invoice[]>([]);
+    const [invoices, setInvoices] = useState([]);
 
     const handleSelect = (invoice: Invoice) => {
         if (selectedInvoices.some((i) => i.id === invoice.id)) {
@@ -98,6 +82,7 @@ export default function TransactionsDetails() {
         0
     );
 
+    // Search criteria action
     const [selectedOption, setSelectedOption] = useState('');
     const selectRef = useRef<HTMLDivElement>(null);
 
@@ -107,6 +92,8 @@ export default function TransactionsDetails() {
             selectRef.current.click(); // Ferme le menu déroulant
         }
     };
+    console.log("invoices",invoices)
+
     return (
         <div className='max-w-screen-2xl mx-auto w-full pb-10 -mt-24'>
             <div className="grid grid-cols-1 lg:grid-cols-4 md:gap-8 pb-2 mb-8">
@@ -138,19 +125,55 @@ export default function TransactionsDetails() {
                             </div>
                             {
                                 selectedOption === 'regroup'
-                                && <SearchForm key="regroup" label="Regroup number" placeholder="..." />
+                                &&
+                                <SearchByRegroupForm
+                                    key="regroup"
+                                    label="Regroup number"
+                                    placeholder="value"
+                                    setInvoices={setInvoices}
+                                    setIsFirstView={setIsFirstView}
+                                    setError={setSearchError}
+                                    setIsPending={setSearchIsLoading}
+                                />
                             }
                             {
                                 selectedOption === 'codecli'
-                                && <SearchForm key="invoice" label="Code client" />
+                                &&
+                                <SearchByCodeCliForm
+                                    key="codecli"
+                                    label="client number"
+                                    placeholder="value"
+                                    setInvoices={setInvoices}
+                                    setIsFirstView={setIsFirstView}
+                                    setError={setSearchError}
+                                    setIsPending={setSearchIsLoading}
+                                />
                             }
                             {
                                 selectedOption === 'contract'
-                                && <SearchForm key="invoice" label="Contract number" />
+                                &&
+                                <SearchByContractForm
+                                    key="contract"
+                                    label="Contract number"
+                                    placeholder="value"
+                                    setInvoices={setInvoices}
+                                    setIsFirstView={setIsFirstView}
+                                    setError={setSearchError}
+                                    setIsPending={setSearchIsLoading}
+                                />
                             }
                             {
                                 selectedOption === 'invoice'
-                                && <SearchForm key="invoice" label="Bill number" />
+                                &&
+                                <SearchByInvoiceForm
+                                    key="invoice"
+                                    label="Bill number"
+                                    placeholder="value"
+                                    setInvoices={setInvoices}
+                                    setIsFirstView={setIsFirstView}
+                                    setError={setSearchError}
+                                    setIsPending={setSearchIsLoading}
+                                />
                             }
 
                         </div>
@@ -163,11 +186,17 @@ export default function TransactionsDetails() {
                             <CardTitle className="text-2xl line-clamp-1">
                                 {`Reference ACI : ${data?.reference ?? ""}`}
                             </CardTitle>
-                            <CardDescription className=" line-clamp-1">
-                                {`Customer : ${data?.name ?? ""} , Amount  :  ${data?.amount ?? ""}
-                                , payment_mode  :  ${data?.payment_mode ?? ""}
-                                 ${data?.bank ?? ""} ${data?.bank ?? ""}`}
-                            </CardDescription>
+                            {
+                                isLoading ?
+                                    (<Skeleton className="w-[500px] h-[20px] rounded-full" />) :
+                                    (<CardDescription className="line-clamp-1 flex gap-3">
+                                        <div>Customer : <span className="font-bold text-md">{data?.name ?? ""}</span></div>
+                                        <div>Amount  :  xfa  <span className="font-bold text-md">{data?.amount}</span></div>
+                                        <div>Date  :   <span className="font-bold text-md">{data?.amount}</span></div>
+                                        <div>Bank :  <span className="font-bold">{data?.bank.name}</span></div>
+                                    </CardDescription>)
+                            }
+
                         </div>
 
                     </CardHeader>
@@ -176,43 +205,63 @@ export default function TransactionsDetails() {
                             direction="horizontal"
                             className="rounded-lg border"
                         >
-                            <ResizablePanel defaultSize={50}>
-                                <ScrollArea className="flex h-[700px] items-center justify-center p-6 rounded-md border">
-                                    <div className="p-3">
-                                        <h4 className="mb-4 text-sm font-medium leading-none">Tags</h4>
-                                        <Table>
-                                            <TableHeader>
-                                                <TableRow>
-                                                    <TableHead className=""></TableHead>
-                                                    <TableHead>regroup</TableHead>
-                                                    <TableHead>contract</TableHead>
-                                                    <TableHead className="">invoice</TableHead>
-                                                    <TableHead className="">Name</TableHead>
-                                                    <TableHead className="">Date</TableHead>
-                                                    <TableHead className="">Amount</TableHead>
-                                                </TableRow>
-                                            </TableHeader>
-                                            <TableBody>
-                                                {[1, 2, 3, 5, 6, 9, 10, 11].map((invoice, i) => (
-                                                    <TableRow key={i++}>
-                                                        <TableCell className="font-medium">
-                                                            <Checkbox key={i} value={i} />
-                                                        </TableCell>
-                                                        <TableCell className="font-medium"></TableCell>
-                                                        <TableCell className="font-medium">200050122</TableCell>
-                                                        <TableCell className="font-medium">200072655</TableCell>
-                                                        <TableCell>DONGMO SABINE</TableCell>
-                                                        <TableCell>25/06/2010</TableCell>
-                                                        <TableCell>797</TableCell>
-                                                    </TableRow>
-                                                ))}
-                                            </TableBody>
-                                        </Table>
-                                    </div>
+                            <ResizablePanel defaultSize={100}>
+                                <ScrollArea className="flex h-[700px] items-center justify-center p-6 rounded-md">
+                                    {
+                                        searchIsLoading ? <LoadingCard /> :
+                                            isFirstView ? <DefaultCard /> :
+                                                <DataTable
+                                                    columns={columns}
+                                                    data={invoices}
+                                                    filterKey={"3"}
+                                                    onSubmit={(row: any[]) => {
+                                                        const ids = row.map((r: any) => r.original.invoice);
+                                                        // deleteTransactionsQuery.mutate({ ids });
+                                                    }}
+                                                    disabled={false}
+                                                />
+                                    }
+                                    {
+
+
+                                        //     !!invoices.length && (
+                                        // <div className="p-3">
+                                        //     <h4 className="mb-4 text-sm font-medium leading-none">Tags</h4>
+                                        //     <Table>
+                                        //         <TableHeader>
+                                        //             <TableRow>
+                                        //                 <TableHead className=""></TableHead>
+                                        //                 <TableHead>regroup</TableHead>
+                                        //                 <TableHead>contract</TableHead>
+                                        //                 <TableHead className="">invoice</TableHead>
+                                        //                 <TableHead className="">Name</TableHead>
+                                        //                 <TableHead className="">Date</TableHead>
+                                        //                 <TableHead className="">Amount</TableHead>
+                                        //             </TableRow>
+                                        //         </TableHeader>
+                                        //         <TableBody>
+                                        //             {invoices.map((invoice, i) => (
+                                        //                 <TableRow key={i++}>
+                                        //                     <TableCell className="font-medium">
+                                        //                         <Checkbox key={i} value={i} />
+                                        //                     </TableCell>
+                                        //                     <TableCell className="font-medium">{invoice[0]}</TableCell>
+                                        //                     <TableCell className="font-medium">{invoice[1]}</TableCell>
+                                        //                     <TableCell className="font-medium">{invoice[2]}</TableCell>
+                                        //                     <TableCell className="font-medium">{invoice[3]}</TableCell>
+                                        //                     <TableCell className="font-medium">{invoice[4]}</TableCell>
+                                        //                     <TableCell className="font-medium">{invoice[5]}</TableCell>
+                                        //                 </TableRow>
+                                        //             ))}
+                                        //         </TableBody>
+                                        //     </Table>
+                                        // </div>
+                                        // )
+                                    }
                                 </ScrollArea>
                             </ResizablePanel>
                             <ResizableHandle />
-                            <ResizablePanel defaultSize={50}>
+                            <ResizablePanel defaultSize={0}>
                                 <ResizablePanelGroup direction="vertical">
                                     <ResizablePanel defaultSize={25}>
                                         <div className="flex h-full items-center justify-center p-6">
@@ -269,3 +318,27 @@ export default function TransactionsDetails() {
     )
 }
 
+
+export const LoadingCard = () => {
+    return (
+        <Card className='border-none drop-shadow-sm'>
+            <CardHeader>
+                <Skeleton className="w-48 h-8" />
+            </CardHeader>
+            <CardContent className='h-[500px] w-full flex items-center justify-center'>
+                <Loader2 className='size-6 text-slate-300 animate-spin' /> Searching...
+            </CardContent>
+        </Card>
+    )
+}
+
+export const DefaultCard = () => {
+    return (
+        <Card className='border-none drop-shadow-sm'>
+            <CardContent className='h-[500px] w-full flex flex-col items-center text-xl text-bold justify-center'>
+                <div className="text-2xl"><BiSearch size={52} className="sm:w-15" /></div>
+                <div>Just Search what you want</div>
+            </CardContent>
+        </Card>
+    )
+}
